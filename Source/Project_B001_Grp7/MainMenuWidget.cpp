807ea11de4b6ebcc8e8ab195cea90c270e6c7d65
AokoMitsUwU <80577298+AokoMitsuu@users.noreply.github.com>
2023-01-30 10:53:44 +0100

    commit

--- Source/Project_B001_Grp7/MainMenuWidget.cpp
+++ Source/Project_B001_Grp7/MainMenuWidget.cpp
@@ -1,5 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "Kismet/GameplayStatics.h"
+#include "Components/Button.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "MainMenuWidget.h"
 
+void UMainMenuWidget::NativeConstruct()
+{
+	if (PlayButton) {
+		PlayButton->OnClicked.AddDynamic(this, &UMainMenuWidget::PlayLevel);
+	}
+
+	if (QuitButton) {
+		QuitButton->OnClicked.AddDynamic(this, &UMainMenuWidget::QuitLevel);
+	}
+}
+
+void UMainMenuWidget::PlayLevel()
+{
+	UGameplayStatics::OpenLevel(GetWorld(), LevelName);
+}
+
+void UMainMenuWidget::QuitLevel()
+{
+	UKismetSystemLibrary::QuitGame(GetWorld(), GetWorld()->GetFirstPlayerController(), EQuitPreference::Quit, false);
+}
--- Source/Project_B001_Grp7/MainMenuWidget.h
+++ Source/Project_B001_Grp7/MainMenuWidget.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
 #include "MainMenuWidget.generated.h"
 
 /**
@@ -14,4 +16,20 @@ class PROJECT_B001_GRP7_API UMainMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+	virtual void NativeConstruct() override;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+		class UButton* PlayButton;
+
+	UFUNCTION()
+		void PlayLevel();
+
+	UPROPERTY(EditAnywhere)
+		FName LevelName;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+		class UButton* QuitButton;
+
+	UFUNCTION()
+		void QuitLevel();
 };
