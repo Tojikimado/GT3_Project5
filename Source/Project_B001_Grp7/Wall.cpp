2d26d9b72851e8bf731d0341c3d98d1d8092531a
AokoMitsUwU <80577298+AokoMitsuu@users.noreply.github.com>
2023-02-01 15:15:31 +0100

    push wall

---
+++ Source/Project_B001_Grp7/Wall.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Wall.h"
+
+// Sets default values
+AWall::AWall()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	WallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision Box"));
+	CollisionBox->SetCollisionProfileName("Trigger");
+
+	CollisionBox->AttachToComponent(WallMesh, FAttachmentTransformRules::KeepRelativeTransform);
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &AWall::OverlapBegin);
+
+	PointCostText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("PointCostText"));
+
+	PointCostText->AttachToComponent(WallMesh, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+// Called when the game starts or when spawned
+void AWall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FString textCost = "Points : ";
+	textCost.Append(FString::FromInt(Cost));
+
+	PointCostText->SetText(FText::FromString(textCost));
+}
+
+// Called every frame
+void AWall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AWall::OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->IsA(AProject_B001_Grp7Character::StaticClass())) {
+
+		if (AProject_B001_Grp7Character* Player = Cast<AProject_B001_Grp7Character>(OtherActor)) {
+			if (Player->Points >= Cost) {
+				K2_DestroyActor();
+			}
+		}
+	}
+}
+
+
---
+++ Source/Project_B001_Grp7/Wall.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/TextRenderComponent.h"
+#include "Project_B001_Grp7Character.h"
+#include "Wall.generated.h"
+
+UCLASS()
+class PROJECT_B001_GRP7_API AWall : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AWall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* WallMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+		UBoxComponent* CollisionBox;
+
+	UPROPERTY(EditAnywhere)
+		int Cost;
+
+	UFUNCTION()
+		void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY(EditAnywhere)
+		UTextRenderComponent* PointCostText;
+};
